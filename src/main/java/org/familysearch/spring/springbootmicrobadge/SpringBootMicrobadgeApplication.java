2afc915c87ad8f9267a2c2108ab3c90541d7ae42
Jared Daniels <danielsj@familysearch.org>
2017-09-22 14:40:54 -0600

    Initial creation of application from start.spring.io

---
+++ src/main/java/org/familysearch/spring/springbootmicrobadge/SpringBootMicrobadgeApplication.java
@@ -0,0 +1,12 @@
+package org.familysearch.spring.springbootmicrobadge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicrobadgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicrobadgeApplication.class, args);
+	}
+}
